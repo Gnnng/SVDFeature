0de5586831ecabe461d8913b0627860c46c20e87
Gnnng <gnnnnng@gmail.com>
2014-05-16 00:52:08 +0800

    New run.sh

--- art/run.sh
+++ art/run.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
 # example running script for art
 
-# make buffer, transform text format to binary format
-../tools/make_feature_buffer art.base art.base.buffer
-../tools/make_feature_buffer art.base art.test.buffer
-
 # default value
-round_val=60
+round_val=100
 factor_val=3
 lambda=0.004
-
+learning=0.005
 # arguments from shell
 argv1=$1
 argv2=$2
 argv3=$3
+argv4=$4
 
-if [ ${#argv1} -gt 0 ]; then
-	round_val=$argv1
-fi
+echo $argv1
 
-if [ ${#argv1} -gt 0 ]; then
-	factor_val=$argv2
-fi
+if [ ${#argv1} -gt 0 ]; then round_val=$argv1; fi
+if [ ${#argv1} -gt 0 ]; then factor_val=$argv2; fi
+if [ ${#argv3} -gt 0 ]; then lambda=$argv3; fi
+if [ ${#argv4} -gt 0 ]; then learning=$argv4; fi
 
-if [ ${#argv3} -gt 0 ]; then
-	lambda=$argv3
-fi
+echo Parameter list
+echo round is $round_val
+echo num_factor is $factor_val
+echo lambda is $lambda
+echo learning_rate is $learning
 
+# make buffer, transform text format to binary format
+../tools/make_feature_buffer art.base art.base.buffer
+../tools/make_feature_buffer art.base art.test.buffer
 # training for $round_val rounds
-../svd_feature art.conf num_round=$round_val num_factor=$factor_val wd_user=$lambda wd_item=$lambda
+../svd_feature art.conf num_round=$round_val num_factor=$factor_val wd_user=$lambda wd_item=$lambda learning_rate=$learning
 # write out prediction from model
-../svd_feature_infer art.conf pred=$round_val num_facotr=$factor_val wd_user=$lambda wd_item=$lambda
+../svd_feature_infer art.conf pred=$round_val num_facotr=$factor_val wd_user=$lambda wd_item=$lambda learning_rate=$learning
